88e6811f28ba06c6049557a79a85df16c5bf7290
rohangangaraju <gangaraju.rohan@gmail.com>
2020-04-01 02:53:06 -0400

    Menubar and Navbar positioning and coloring

---
+++ navbar.js
@@ -0,0 +1,7 @@
+function onMouseEnter() {
+    document.getElementById('sidebar').style.width = '250%';
+}
+
+function onMouseExit() {
+    document.getElementById('sidebar').style.width = '70%';
+}

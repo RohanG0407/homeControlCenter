8d4012f4b9c25e7c19bde1438733bd61530de95d
rohangangaraju <gangaraju.rohan@gmail.com>
2020-04-01 14:27:00 -0400

    Debug sidebar

--- navbar.js
+++ navbar.js
@@ -1,12 +1,19 @@
+const element = document.querySelector('#screen');
+const style = getComputedStyle(element)
+console.log(style.width);
+console.log(style.height);
+
 function onMouseEnter() {
     document.getElementById('sidebar').style.width = '250%';
     document.getElementById('menubar-item').innerHTML = "<img src=\"./assets/menu-2.png\" width=\"40\" height=\"40\" alt=\"\" class=\"d-inline-block align-top\" style=\"margin-right: 10%;\"> Sidebar"
+    document.getElementsByClassName('sidebar-item')[0].style.width = "200%";
     document.getElementsByClassName('sidebar-item')[0].innerHTML = "<p><img src=\"./assets/home.png\" width=\"40\" height=\"40\" alt=\"\" class=\"sidebar-item-icon\">Some text</p>"
     }
 
 function onMouseExit() {
     document.getElementById('sidebar').style.width = '70%';
     document.getElementById('menubar-item').innerHTML = "<img src=\"./assets/menu-2.png\" width=\"40\" height=\"40\" alt=\"\" class=\"d-inline-block align-top\" style=\"margin-right: 10%;\"> Dashboard"
+    //document.getElementsByClassName('sidebar-item')[0].style.width = "50px";
     document.getElementsByClassName('sidebar-item')[0].innerHTML = "<p><img src=\"./assets/home.png\" width=\"40\" height=\"40\" alt=\"\" class=\"sidebar-item-icon\"></p>"
 
 }
